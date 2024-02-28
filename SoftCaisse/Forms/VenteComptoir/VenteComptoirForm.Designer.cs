a49f6bab39bea92dd56ade00e98a50d817e95a5d
Jakina24K <jakinaandriantsoa@gmail.com>
2024-02-28 09:56:08 +0300

    Jakina Last Update

--- SoftCaisse/Forms/VenteComptoir/VenteComptoirForm.Designer.cs
+++ SoftCaisse/Forms/VenteComptoir/VenteComptoirForm.Designer.cs
@@ -605,15 +605,10 @@ namespace SoftCaisse.Forms.VenteComptoir
             // 
             this.comboBoxDeviseEnregistrement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxDeviseEnregistrement.FormattingEnabled = true;
-            this.comboBoxDeviseEnregistrement.Items.AddRange(new object[] {
-            "Ariary",
-            "Euro",
-            "Yen"});
             this.comboBoxDeviseEnregistrement.Location = new System.Drawing.Point(357, 14);
             this.comboBoxDeviseEnregistrement.Name = "comboBoxDeviseEnregistrement";
             this.comboBoxDeviseEnregistrement.Size = new System.Drawing.Size(148, 24);
             this.comboBoxDeviseEnregistrement.TabIndex = 3;
-            this.comboBoxDeviseEnregistrement.Text = "Euro";
             this.comboBoxDeviseEnregistrement.SelectedIndexChanged += new System.EventHandler(this.comboBoxDeviseEnregistrement_SelectedIndexChanged);
             // 
             // textBoxLibelleEnregistrement
